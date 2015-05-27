474c03280ac3586faa43d7c9100cc9595375be76
alcexhim <alcexhim@gmail.com>
2015-05-27 09:07:53 -0400

    Additional constructor parameters for Verb

--- Libraries/Alyx.Linguistics/LanguageParts/Verb.cs
+++ Libraries/Alyx.Linguistics/LanguageParts/Verb.cs
@@ -13,7 +13,13 @@ namespace Alyx.Linguistics.LanguageParts
 
 		}
 
-		public Verb(Guid id) : base(id) { }
+		public Verb(Guid id, Person person = Linguistics.Person.Unspecified, Tense tense = Linguistics.Tense.Unspecified, Aspect aspect = Linguistics.Aspect.Unspecified)
+			: base (id)
+		{
+			mvarPerson = person;
+			mvarTense = tense;
+			mvarAspect = aspect;
+		}
 
 		private Person mvarPerson = Person.Unspecified;
 		public Person Person { get { return mvarPerson; } set { mvarPerson = value; } }
