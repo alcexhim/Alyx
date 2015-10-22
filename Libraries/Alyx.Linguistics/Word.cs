1efac5a5b1ff178e4a587d99922fbc48f99c6ba4
alcexhim <alcexhim@gmail.com>
2015-10-22 15:38:43 -0400

    Implement search for word by value (highly unoptimized...)

--- Libraries/Alyx.Linguistics/Word.cs
+++ Libraries/Alyx.Linguistics/Word.cs
@@ -21,6 +21,85 @@ namespace Alyx.Linguistics
 					return null;
 				}
 			}
+
+			private Dictionary<string, Word> wordsByValue = new Dictionary<string, Word>();
+
+			public Word this[string value]
+			{
+				get
+				{
+					value = value.ToLower();
+
+					if (!wordsByValue.ContainsKey(value))
+					{
+						bool exitAll = false;
+
+						foreach (Word word in this)
+						{
+							if (word.Value == value)
+							{
+								wordsByValue.Add(value, word);
+								break;
+							}
+							else
+							{
+								foreach (WordMapper mapper in Language.CurrentLanguage.WordMappers)
+								{
+									bool exit = false;
+									if (mapper.Condition == null || mapper.Condition.Test
+									(
+										new KeyValuePair<string, object>("Word", word),
+										new KeyValuePair<string, object>("WordClasses", word.Classes),
+										new KeyValuePair<string, object>("ID", word.ID.ToString("B").ToUpper())
+									))
+									{
+										foreach (WordMapperMapping mapping in mapper.Mappings)
+										{
+											string[] mappingValueParts = mapping.Value.Split(new string[] { "$(Word)" }, StringSplitOptions.None);
+											if (mappingValueParts.Length >= 2)
+											{
+												if (String.IsNullOrEmpty(mappingValueParts[0]) && String.IsNullOrEmpty(mappingValueParts[mappingValueParts.Length - 1]))
+												{
+													if (mapping.Value.Replace("$(Word)", word.Value) == value)
+													{
+														wordsByValue.Add(value, word);
+														exit = true;
+														break;
+													}
+												}
+												else if ((!String.IsNullOrEmpty(mappingValueParts[0]) && value.StartsWith(mappingValueParts[0]))
+													|| (!String.IsNullOrEmpty(mappingValueParts[mappingValueParts.Length - 1]) && value.EndsWith(mappingValueParts[mappingValueParts.Length - 1])))
+												{
+													string wordValue = value.Substring(mappingValueParts[0].Length, value.Length - mappingValueParts[mappingValueParts.Length - 1].Length);
+													if (word.Value == wordValue)
+													{
+														wordsByValue.Add(value, word);
+														exit = true;
+														break;
+													}
+												}
+											}
+											else if (mappingValueParts.Length == 1)
+											{
+												if (mapping.Value == value) return word;
+											}
+										}
+									}
+									if (exit)
+									{
+										exitAll = true;
+										break;
+									}
+								}
+							}
+							if (exitAll) break;
+						}
+					}
+
+					if (wordsByValue.ContainsKey(value)) return wordsByValue[value];
+					return null;
+				}
+			}
 		}
 
 		private Guid mvarID = Guid.Empty;
@@ -41,6 +120,15 @@ namespace Alyx.Linguistics
 		public Word(Guid id)
 		{
 			mvarID = id;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(mvarID.ToString("B").ToUpper());
+			sb.Append(' ');
+			sb.Append(mvarValue);
+			return sb.ToString();
 		}
 	}
 }
