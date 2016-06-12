e2cb24795e589ce89597b9f0183afcba4a08220e
Michael Becker <alcexhim@gmail.com>
2016-06-12 00:23:22 -0400

    I don't know where I'm going with this

--- Libraries/Alyx.Linguistics/SentenceParsers/V2/DefaultSentenceParserV2.cs
+++ Libraries/Alyx.Linguistics/SentenceParsers/V2/DefaultSentenceParserV2.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 
+using Alyx.Linguistics.LanguageParts;
+
 namespace Alyx.Linguistics.SentenceParsers.V2
 {
 	public class DefaultSentenceParserV2 : SentenceParser
@@ -24,10 +26,59 @@ namespace Alyx.Linguistics.SentenceParsers.V2
 				candidates.Add (candidate);
 			}
 
-			for (int i = 0; i < candidates.Count; i++) {
-				if (candidates [i].Instances.ContainsClass (WordClasses.Article)) {
-					// next candidate is either adjective or noun
+			for (int iw = 0; iw < candidates.Count; iw++) {
+				foreach (WordPattern pattern in Language.CurrentLanguage.WordPatterns)
+				{
+					bool patternWrong = false;
 
+					for (int ip = 0; ip < pattern.Parts.Count; ip++) {
+						if (pattern.Parts [ip].WordClass != null) {
+							if (!(candidates [iw].Instances.ContainsClass (pattern.Parts [ip].WordClass))) {
+								if (!pattern.Parts [ip].IsOptional) {
+									// the currently-selected pattern part is not optional, so if we lose here our pattern is wrong
+									if (iw + 1 < candidates.Count) {
+										// we are not the last candidate, so check to see if the next candidate has an instance
+										if (candidates [iw].Instances.Count == 0 && candidates [iw + 1].Instances.Count > 0) {
+											// our current candidate doesn't have a instance, but the next candidate does
+
+											if (!(candidates [iw + 1].Instances.ContainsClass (pattern.Parts [ip].WordClass))) {
+												// next candidate has a WordInstance which doesn't match the pattern
+												// so assume our current candidate is an unknown and create a WordInstance for it
+
+												if (pattern.Parts [ip].WordClass == WordClasses.Noun) {
+													Word word = new Word (Guid.NewGuid ());
+													word.Value = candidates [iw].Value;
+													word.Sources.Add (Language.CurrentLanguage.WordSources [WordSourceGuids.Learned]);
+													word.Classes.Add (WordClasses.Noun);
+													Language.CurrentLanguage.Words.Add (word);
+
+													NounInstance inst = new NounInstance (word);
+													candidates [iw].Instances.Add (inst);
+													candidates [iw].PreferredInstance = inst;
+
+													if (candidates [iw - 1].PreferredInstance is ArticleInstance) {
+														inst.Article = (candidates [iw - 1].PreferredInstance as ArticleInstance);
+													}
+													continue;
+												}
+											}
+										}
+									}
+									patternWrong = true;
+									break;
+								}
+							} else {
+								WordInstance[] insts = candidates [iw].Instances.GetByClass (pattern.Parts [ip].WordClass);
+								if (insts.Length > 0) {
+									candidates [iw].PreferredInstance = insts [0];
+									iw++;
+								}
+							}
+						}
+					}
+
+					if (patternWrong)
+						continue;
 				}
 			}
 		}
